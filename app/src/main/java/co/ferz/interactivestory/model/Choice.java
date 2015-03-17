3422fea19f7ef04b5dfed2c9ae68f68d569c7f26
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-03-16 21:09:02 -0500

    THInteractiveStoryMiniApp
    Treehouse's story app that shows the basics of MVC, user input, "finite
    state machines" concept with story boards, etc.

---
+++ app/src/main/java/co/ferz/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package co.ferz.interactivestory.model;
+
+/**
+ * Created on 3/14/2015.
+ */
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+}
